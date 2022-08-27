4b7bf5093a4b5e1912b010c8ec2145108860dee6
Akash_Harale <haraleakash2020@gmail.com>
2022-08-27 10:02:44 +0530

    mintty.exe.stackdump added

---
+++ assignment_01.py
@@ -0,0 +1,54 @@
+# Q1) Write a command to get the Python version you are using.
+from re import A
+import sys
+print(sys.version)
+
+print("-" *100)
+
+# Q2) Write a python script to print Hello Python on the screen directly in the command line
+print("Hello Python")  # ----> How to upload answers which I have done in command line
+
+print("-" *100)
+
+# Q3) Write a python script and store it in a file and execute the file from the command line.
+print("Hello world")  # ---->  I have solved this question but I how can I upload the answer of this question on github
+
+print("-" *100)
+
+# Q4) Write a python script to print Hello Python on the screen using visual studio code.
+print("Hello Python") 
+
+print("-" *100)
+
+# Q5) Write a python script to print Hello on the first line and Python on the second line
+print("Hello\nPython")
+
+print("-" *100)
+
+# Q6) Write a python script to print “MySirG” on the screen
+print("MySirG")
+
+print("-" *100)
+
+# Q7) Write a python script to print “Teacher’s Day” on the screen
+print("Teacher's Day")
+
+print("-" *100)
+
+# Q8) Write a python script to print the value of a variable. Variable contains your name as data
+a="Akash Harale"
+print(a)
+
+print("-" *100)
+
+# Q9) Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name, age, qualification, yrOfExp = "Akash", 24 , "Agriculture Engg.", "Fresher"
+print(name, age, qualification, yrOfExp)
+
+print("-" *100)
+
+# Q10)  Write a python script to print values of three variables, each in a new line. Variable contains integer values
+a,b,c= 4,8,6
+print(a,"\n",b,"\n",c )
+
+
